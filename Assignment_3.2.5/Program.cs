da258ba7af7d4665bc47fcabc27927efd0d57b52
tjdour <tongjdour@outlook.com>
2026-07-22 01:00:42 +0300

    Assignment 3.2.5

---
+++ Assignment_3.2.5/Program.cs
@@ -0,0 +1,42 @@
+﻿//Assignment 2.3.5
+
+//prompt user for array size
+Console.WriteLine("Enter array size: ");
+int arrSize = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[arrSize];
+
+//prompt for values of array indices 
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine($"Enter values of index[{i}]: ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+
+//prompt for number to find
+Console.WriteLine("Enter a number 0-9 to find its index: ");
+int toFind = Convert.ToInt32(Console.ReadLine());
+
+//call function and show result
+int result = FindIndex(numbers, toFind);
+Console.WriteLine(result);
+
+
+
+//find index method
+int FindIndex (int[] numbers, int toFind)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+
+
+        if (numbers[i] == toFind)
+        {
+        return i;
+            
+        }
+
+        //return -1;
+    }
+    return -1;
+}
