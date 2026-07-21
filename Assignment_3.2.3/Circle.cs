266a9de13551406c77f1ae8d7ae40bf66befb46c
tjdour <tongjdour@outlook.com>
2026-07-22 00:59:03 +0300

    Assignment 3.2.3

---
+++ Assignment_3.2.3/Circle.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace Assignment_3._2._3
+{
+    public class Circle
+    {
+        public double Radius { get; set; }
+
+        public double Area
+        {
+            get
+            {
+                return Math.PI * Radius * Radius;
+            }
+        }
+
+        public Circle(double radius)
+        { 
+            Radius = radius;
+        }
+
+        public static double operator +(Circle c1, Circle c2)
+        { 
+            return c1.Area + c2.Area;
+        }
+
+        public static double operator -(Circle c1, Circle c2)
+        {
+            return c1.Area - c2.Area;
+        }
+        
+        
+    }
+}
---
+++ Assignment_3.2.3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Assignment_3._2._3;
+
+Circle circle1 = new Circle(3);
+Circle circle2 = new Circle(5);
+
+double totalArea = circle1 + circle2;
+double areaDifference = circle1 - circle2;
+
+Console.WriteLine($"Circle 1 area: {circle1.Area:F2}");
+Console.WriteLine($"Circle 2 area: {circle2.Area:F2}");
+Console.WriteLine($"Total area: {totalArea:F2}");
+Console.WriteLine($"Area difference: {areaDifference:F2}");
